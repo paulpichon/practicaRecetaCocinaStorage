0588284b69aaf3d37a3b42dd7c75074cbb3afdd1
Paul Pichon F <paul10_barca@hotmail.com>
2023-01-03 15:54:56 -0600

    obtener los resultados a partir de la seleccion del usuario

--- js/app.js
+++ js/app.js
@@ -50,7 +50,9 @@ function iniciarApp() {
         const categoria = e.target.value;
         const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${ categoria }`;
 
-        console.log( url );
+        fetch( url )
+            .then( respuesta => respuesta.json() )
+                .then( resultado => console.log(resultado) )
 
 
     }
