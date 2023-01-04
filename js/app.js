44e827cdcf43c57658353d47d2eec10b98091b30
Paul Pichon F <paul10_barca@hotmail.com>
2023-01-04 14:18:30 -0600

    Renderizando las recetas

--- js/app.js
+++ js/app.js
@@ -1,70 +1,63 @@
-//crear ufuncion para inicar el proyecto
 function iniciarApp() {
 
-    //variable que representa el input donde se mostraran las categorias 
+    //variable que representa select
     const selectCategorias = document.querySelector('#categorias');
-    //agregamos un listener a al select de categorias
-    selectCategorias.addEventListener('change', seleccionarCategoria );
+    //añadir un listener a selectCategorias
+    selectCategorias.addEventListener('change', seleccionarCategoria);
 
-    //llamamos funcion para obtener las categorias
-    obtenerCategorias();
+    //variable que representa donde se mostraran las recetas
+    const resultado = document.querySelector('#resultado');
 
-    //funcion para obtener todas las categorias
-    function obtenerCategorias() {
-        //url para llamar todas las categorias
-        const url = 'https://www.themealdb.com/api/json/v1/1/categories.php';
-        //fetchApi
-        fetch(url)
-            //retornamos usando el metodo de JSON
-            //el return es implicito
-            .then( respuesta => respuesta.json() )
-            //podemos poner el resultado con cualquier nombre en este caso lo ponemos como resultado
-            //pasamos el resultado a la funcion para mostrarCategorias
-            .then( resultado => mostrarCategorias( resultado.categories ) )
+    //llamar funcion
+    cargarCategorias();
 
-    }
-
-    //funcion para mostrarCategorias
-    //le pasamos como parametro las categorias y lo definimos como un arreglo vacio
-    function mostrarCategorias( categorias = []) {
-        //meidante un foreach accedemos a cada uno de los elementos
-        categorias.forEach( categoria => {
-            //destructuring
-            const {strCategory} = categoria;
-            //crear el html del option
-            const option = document.createElement('OPTION');
-            //valor del option que se va a leer una vez el usuario haya elegido una option
-            option.value = strCategory;
-            //textcontent
-            option.textContent = strCategory;
-            //renderizar
-            selectCategorias.appendChild( option );
-
-
-        });
-    }
-
-    //funcion para seleccionar categoria
-    function seleccionarCategoria( e ) {
-        //categoria seleccionada
-        const categoria = e.target.value;
-        const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${ categoria }`;
+    //funcion cargar categorias
+    function cargarCategorias() {
+        //url
+        const url = "https://www.themealdb.com/api/json/v1/1/categories.php";
 
         fetch( url )
             .then( respuesta => respuesta.json() )
-                .then( resultado => mostrarRecetas(resultado.meals) )
+                .then( resultado => mostrarCategorias( resultado.categories ) )
+    }
 
+    //funcion mostrar categorias en el select
+    function mostrarCategorias( categorias ) {
+        
+        //iterar
+        categorias.forEach(categoria => {
+            //destruturing
+            const { strCategory } = categoria;
+            
+            //html
+            const option = document.createElement('OPTION');
+            option.value = strCategory;
+            option.textContent = strCategory;
+            //renderizar
+            selectCategorias.appendChild( option );
+        });
 
     }
 
-    //funcion para mostrar los resultados por categoria
-    function mostrarRecetas( recetas = [] ) {
-        
-        //iterar en cada uno de los resultados
-        recetas.forEach(receta => {
+    //funcion para seleccionar una categoria
+    function seleccionarCategoria( e ) {
+        const categoria = e.target.value;
+        const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${ categoria }`;
+
+        fetch( url ) 
+            .then( respuesta => respuesta.json() )
+                .then( resultado => mostrarRecetas( resultado.meals ) )
+
+    }
+
+    //funcion para mostrar recetas
+    function mostrarRecetas( recetas = []) {
+        //iterar el arreglo de recetas
+        recetas.forEach( receta => {
             //destructuring
             const { idMeal, strMeal, strMealThumb } = receta;
-            //construir el html
+
+            //html
             //contenedor
             const recetaContenedor = document.createElement('DIV');
             recetaContenedor.classList.add('col-md-4');
@@ -75,22 +68,43 @@ function iniciarApp() {
 
             //imagen
             const recetaImagen = document.createElement('IMG');
-            //estilos
             recetaImagen.classList.add('card-img-top');
-            //Alt de la imagen
-            recetaImagen.alt = `Imagen de la receta ${ strMeal }`;
-            //source de la imagen
+            recetaImagen.alt = `La imagen de la receta ${strMeal}`;
             recetaImagen.src = strMealThumb;
 
-            //cuerpo del body
+            /***************CARD BODY****************/
+            //card body
             const recetaCardBody = document.createElement('DIV');
             recetaCardBody.classList.add('card-body');
 
-            console.log( recetaImagen );
+            //heading
+            const recetaHeading = document.createElement('H3');
+            recetaHeading.classList.add('card-title', 'mb-3');
+            recetaHeading.textContent = strMeal;
+
+            //button
+            const recetaButton = document.createElement('BUTTON');
+            recetaButton.classList.add('btn', 'btn-danger', 'w-100');
+            recetaButton.textContent = 'Ver Receta';
+
+            //inyectar en el codigo HTML
+            recetaCardBody.appendChild( recetaHeading );
+            recetaCardBody.appendChild( recetaButton );
+
+            //receta card
+            recetaCard.appendChild( recetaImagen );
+            recetaCard.appendChild( recetaCardBody );
+
+            //agregar al contenedor principal
+            recetaContenedor.appendChild( recetaCard );
+
+            //renderizar
+            resultado.appendChild( recetaContenedor );
+
         });
     }
 
-}   
+}
 
-//inicar funcion iniciarApp()
+
 document.addEventListener('DOMContentLoaded', iniciarApp);
